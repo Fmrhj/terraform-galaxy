d3b36f5c683cebc2baa0bf41b9610f101f2bb458
Fernando Zepeda <fernando.zepeda@xing.com>
2021-11-06 00:28:36 +0100

    change instance to ubuntu

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,9 @@
 resource "vultr_instance" "my_instance" {
   plan   = "vc2-1c-1gb"
-  os_id  = "167"
+  os_id  = "270"
   region = var.vultr_region
+}
+
+resource "docker_image" "galaxy" {
+  name = var.docker_image
 }
