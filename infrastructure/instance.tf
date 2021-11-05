3977d3f7fb5670ed0e7a38bf3751931ad6ba21ce
Fernando Zepeda <fernando.zepeda@xing.com>
2021-11-06 00:48:06 +0100

    new ubuntu image

--- infrastructure/instance.tf
+++ infrastructure/instance.tf
@@ -12,9 +12,17 @@ terraform {
       source  = "vultr/vultr"
       version = "2.5.0"
     }
+
   }
 }
 
 provider "vultr" {
 }
+
+resource "vultr_instance" "my_instance" {
+  plan   = "vc2-1c-1gb"
+  os_id  = "270"
+  region = var.vultr_region
+}
+
 
--- infrastructure/main.tf
+++
@@ -1,9 +0,0 @@
-resource "vultr_instance" "my_instance" {
-  plan   = "vc2-1c-1gb"
-  os_id  = "270"
-  region = var.vultr_region
-}
-
-resource "docker_image" "galaxy" {
-  name = var.docker_image
-}
